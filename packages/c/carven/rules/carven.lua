890ea4fe55b121b559e86e05e220dd3e3f5650a9
ryblust <zimingzhu1111@gmail.com>
2026-07-11 04:21:30 +0800

    Update carven.lua

--- packages/c/carven/rules/carven.lua
+++ packages/c/carven/rules/carven.lua
@@ -6,20 +6,24 @@ rule("carven.build")
         if #sourcekinds == 0 then
             table.insert(sourcekinds, "cxx")
         end
+
+        local carven_package = target:pkg("carven")
+        target:add("includedirs", path.join(carven_package:installdir(), "include"))
     end)
 
     on_buildcmd_file(function (target, batchcmds, sourcefile_cv, opt)
-        local carven_pkg = target:pkg("carven")
-        local carven = assert(carven_pkg, "please add_packages(\"carven\") before add_rules(\"@carven/carven\")")
-        local carven_program = path.join(carven:installdir(), "bin", is_host("windows") and "carven.exe" or "carven")
+        local carven_package = target:pkg("carven")
+        local carven_program = path.join(carven_package:installdir(), "bin", is_host("windows") and "carven.exe" or "carven")
         local sourcefile_cpp = target:autogenfile((sourcefile_cv:gsub("%.cv$", ".cpp")))
         local objectfile = target:objectfile(sourcefile_cpp)
 
         table.insert(target:objectfiles(), objectfile)
+
         batchcmds:show_progress(opt.progress, "${color.build.object}transpiling.cv %s", sourcefile_cv)
         batchcmds:mkdir(path.directory(sourcefile_cpp))
         batchcmds:vrunv(carven_program, {"transpile", "-o", path(target:autogendir()), path(sourcefile_cv)})
         batchcmds:compile(sourcefile_cpp, objectfile)
+
         batchcmds:add_depfiles(sourcefile_cv)
         batchcmds:add_depvalues(carven_program)
         batchcmds:set_depmtime(os.mtime(objectfile))
